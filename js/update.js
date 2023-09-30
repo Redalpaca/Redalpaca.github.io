08e504d7a51b076e6eb7b3677aafd6b2b3bb96de
Redalpaca <zhoualpaca@gmail.com>
2023-09-30 21:07:27 +0800

    add Update function successfully

--- js/update.js
+++ js/update.js
@@ -8,65 +8,90 @@ class Updater {
 
     constructor() {
         this.__init__();
+        
+        // this.btn = document.querySelector(".btn-update");
+        // this.btn.onclick = function () {
+        //     this.updateBlogCard;
+        // };
+    }
+
+    __init__(path = "./model/LatestPost.html", async = true) {
+        // initialize vars
         this.cnt_total = 3;
         this.cnt_cur = 3; // no value?
         this.blogCards = "";
         this.parent = document.querySelector(".blog");
-        this.btn = document.querySelector(".btn-update");
-        this.btn.onclick = function () {
-            this.updateBlogCard;
-        };
-    }
-
-    __init__(path = "./model/LatestPost.html", async = true) {
+        this.refer = document.querySelector(".ref-insert");
+        // get outer html
         var url = path;
         var xhr = new XMLHttpRequest();
         var doc = "";
         xhr.open('GET', url, async);
-        xhr.setRequestHeader('schemes', 'https');
         xhr.onreadystatechange = function (doc) {
-            // if (xhr.status === 200) {
-            var responseHTML = xhr.responseText;
-            var parser = new DOMParser();
-            doc = parser.parseFromString(responseHTML, 'text/html');
-            console.log(doc);
-            this.blogCards = doc.querySelectorAll(".blog-card");
-            this.cnt_total = this.blogCards.length;
-            // }
-        };
+            if (xhr.readyState === 4) {
+                var responseHTML = xhr.responseText;
+                var parser = new DOMParser();
+                doc = parser.parseFromString(responseHTML, 'text/html');
+                console.log(doc);
+                this.blogCards = doc.querySelectorAll(".blog-card");
+                this.cnt_total = this.blogCards.length;
+            }
+        }.bind(this); 
+        // bind the func 'onreadystatechange' to instance of the class, 
+        // so 'this' with point to 'updater', not the caller. 
         xhr.send(null);
-        return;
     }
+
+    // updateBlogCard() {
+    //     console.log("detect click");
+    //     if (this.cnt_cur + 3 >= this.cnt_total) {
+    //         for (let i = this.cnt_cur; i < this.cnt_total; i++) {
+    //             var blogCard = this.blogCards[i];
+    //             this.parent.append(blogCard);
+    //         }
+    //         this.handleBorder();
+    //         this.cnt_cur = this.cnt_total;
+    //         return;
+    //     }
+    //     for (let i = this.cnt_cur; i < this.cnt_cur + 3; i++) {
+    //         var blogCard = this.blogCards[i];
+    //         this.parent.append(blogCard);
+    //     }
+    //     this.cnt_cur += 3;
+    //     return;
+    // }
 
     updateBlogCard() {
         console.log("detect click");
-        if (this.cnt_cur + 3 >= this.cnt_total) {
-            this.cnt_cur = this.cnt_total;
-            for (let i = this.cnt_cur; i < this.cnt_total; i++) {
-                var blogCard = this.blogCards[i];
-                this.parent.append(blogCard);
-            }
+        var cnt_next = 0;
+        if(this.cnt_total > this.cnt_cur + 3) {
+            cnt_next = this.cnt_cur + 3;
+        } 
+        else {
+            cnt_next = this.cnt_total;
             this.handleBorder();
-            return;
         }
-        for (let i = this.cnt_cur; i < this.cnt_cur + 3; i++) {
+        for (let i = this.cnt_cur; i < cnt_next; i++) {
             var blogCard = this.blogCards[i];
-            this.parent.append(blogCard);
+            this.parent.insertBefore(blogCard, this.refer);
         }
-        this.cnt_cur += 3;
-        return;
+        this.cnt_cur = cnt_next;
     }
 
     handleBorder() {
         // TODO 
         // hide the button, show message: "You already reach the bottom."
-        console.log("test");
+        let btn = document.querySelector(".btn-update");
+        btn.style.visibility = "hidden";
     }
 }
 
 
 var updater = new Updater();
-
+btn = document.querySelector(".btn-update");
+btn.onclick = function () {
+        updater.updateBlogCard();
+    };
 
 
 

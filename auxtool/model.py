2ea9ad7e0b6a2abb2ee3a3f98d2eda5c8b4416bd
Redalpaca <zhoualpaca@gmail.com>
2023-10-05 22:39:34 +0800

    test for python updater

--- auxtool/model.py
+++ auxtool/model.py
@@ -1,9 +1,20 @@
 from bs4 import BeautifulSoup
 
-def parser(path: str):
-    html_string = open(path, 'r', encoding= 'utf-8').read()
-    soup = BeautifulSoup(html_string, "lxml")
+class Updater_Index(object):
+    def __init__(self, path = '.\model\LatestPost.html'):
+        def parser(path: str):
+            html_string = open(path, 'r', encoding= 'utf-8').read()
+            soup = BeautifulSoup(html_string, "lxml")
+            return soup
+        parser(path)   
+        
+        
+        pass
+    
+    
     pass
+
+
 
 def addContent():
     
